1e41ddc281c32eb8751e03c66e73fb0cf3d1f2b4
Antti Kuosmanen <antti@seravo.fi>
2014-08-01 01:35:16 +0300

    Verbose mode added, some more events

--- kyyla.js
+++ kyyla.js
@@ -9,91 +9,133 @@
 
 var sys = require('sys');
 var util = require('util');
-
 var child_process = require('child_process'),
     exec = child_process.exec,
     spawn = child_process.spawn;
 
+/*
+ * Global variables
+ */
+var args = process.argv;
+var verbose = args.indexOf('-v') != -1
+            || args.indexOf('--verbose') != -1;
 var motion;
+
+
+/*
+ * Config
+ */
 var motioncmd = '/usr/bin/motion';
 var motionconf = './motion.conf';
 
-(function () {
-  
-  /*
-   * Initalization function
-   */
-  function init() {
-    console.log('Kyyla server starting...');
-    
-    // leave process running after script execution
-    process.stdin.resume();
 
-    // start motion
-    startMotion();
+/*
+ * Initalization 
+ */
+function init() {
+  console.log('Kyyla server starting...');
   
+  // leave process running after script execution
+  process.stdin.resume();
+
+  // start motion
+  startMotion();
+}
+
+/*
+ * Helper function that can be passed to exec calls for verbose output
+ */
+function puts(error, stdout, stderr) { 
+  console.log(stderr);
+  if (error !== null) {
+    console.log('exec error: ' + error);
   }
+}
 
-  /*
-   * Helper function that can be passed to exec calls for verbose output
-   */
-  function puts(error, stdout, stderr) { 
-    console.log(stderr);
-    if (error !== null) {
-      console.log('exec error: ' + error);
+/*
+ * Launches motion tracking
+ */
+function startMotion(callback) {
+  console.log('Starting motion...');
+  
+  // spawn child process
+  motion = spawn(
+    motioncmd, 
+    ['-c', motionconf]
+  );
+
+  // event handlers for motion
+  motion.stdout.on('data', function (data) {
+    
+    if(verbose)
+        process.stdout.write(data);
+    
+    if(/event_start/.test(data)) {
+      onMovementStart();
     }
-  }
 
-  /*
-   * Starts motion
-   */
-  function startMotion(callback) {
-    console.log('Starting motion...');
+    if(/event_end/.test(data)) {
+      onMovementEnd();
+    }
     
-    // spawn child process
-    motion = spawn(
-      motioncmd, 
-      ['-c', motionconf]
-    );
+  });
 
-    // event handlers for motion
-    motion.stdout.on('data', function (data) {
-      process.stdout.write(data);
-    });
+  motion.stderr.on('data', function (data) {
+    
+    if(verbose)
+      process.stderr.write(data);
+    
+    if(/Started\ stream\ webcam\ server/.test(data)) {
+      console.log("Ready.")
+    }
 
-    motion.stderr.on('data', function (data) {
-      //process.stdout.write('stderr: ' + data);
-    });
-
-    motion.on('close', function (code) {
-      console.log('motion exited with code ' + code);
-    });
-
-  }
-
-  function stopMotion(callback) {
-    // kill the process
-    motion.kill();
-  }
-
-  /*
-   * Termination handling
-   */
-  process.on('exit', exitHandler.bind(null,{cleanup:true}));
-  process.on('SIGINT', exitHandler.bind(null, {exit:true}));
-  process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
-  function exitHandler(options, err) {
-
-    // clean up
-    if (options.cleanup) {
-      // stop the motion daemon
-      stopMotion();
+    if(/Permission denied/.test(data)) {
+      process.stderr.write(data);
+      console.log('Now exiting...');
       process.exit();
     }
-    
-    if (err) console.log(err.stack);
-    if (options.exit) process.exit();
-  }
 
-  init();
-})();
+  });
+
+  motion.on('close', function (code) {
+    process.stderr.write('motion exited with code ' + code + '\r');
+    process.exit();
+  });
+
+}
+
+/*
+ * Triggers when movement begins
+ */
+function onMovementStart() {
+  console.log("Motion Started!");
+}
+
+/*
+ * No movement for x amount of seconds
+ */
+function onMovementEnd() {
+  console.log("Motion Ended");
+}
+
+/*
+ * Termination handling and clean up
+ */
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+function exitHandler(options, err) {
+
+  // clean up
+  if (options.cleanup) {
+    // stop the motion daemon
+    console.log('Stopping motion and exiting...')
+    motion.kill();
+    process.exit();
+  }
+  
+  if (err) console.log(err.stack);
+  if (options.exit) process.exit();
+}
+
+init();
